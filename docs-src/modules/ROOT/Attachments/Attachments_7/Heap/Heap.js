9e46d2cac16466d063868ac21117e6dfc6413687
ISA-1941 <dsa.github.isa@gmail.com>
2026-06-23 09:08:55 +0300

    Update

--- docs-src/modules/ROOT/Attachments/Attachments_7/Heap/Heap.js
+++ docs-src/modules/ROOT/Attachments/Attachments_7/Heap/Heap.js
@@ -1,16 +1,19 @@
 main();
 function heapSort(arr) {
-    var i, size, temp;
+    var i,step,size, temp;
     size = arr.length;
+    step = 0;
     console.log('size = ', size, 'Begining:', arr);
-    for (i = Math.floor(size / 2) - 1; i >= 0; i--) {
-        console.log('                   i = ', i);
-        console.log('Before heapify:', arr);
+    for (i = Math.floor(size / 2) - 1; i >= 0; i--) {    
+        //console.log('                   i = ', i);
+        //console.log('Before heapify:', arr);
         heapify(arr, size, i);
-        console.log('After heapify:', arr);
+        step++;
+        console.log('step = ', step, 'After heapify:', arr);
     }
-    console.log('size = ', size, 'After heapify:', arr);
+    console.log('After 1 phase:', arr);
     for (i = size - 1; i > 0; i--) {
+        console.log(                     'i =', i, 'Before swap:', arr[0], arr[i]);
         temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
@@ -18,22 +21,23 @@ function heapSort(arr) {
         console.log('size = ', size, 'i =', i, 'heapify:', arr);
     }
 }
-function heapify(arr, size, i) {
+function heapify(arr, sizeI, i) {
     var largest, left, right, temp;
     largest = i;
     left = 2 * i + 1;
     right = 2 * i + 2;
-    if (left < size && arr[left] > arr[largest]) {
+    console.log('     sizeI =', sizeI, 'i =', i, 'left =', left, 'right =', right, '--->', arr[left], '?', arr[largest]);
+    if (left < sizeI && arr[left] > arr[largest]) {
         largest = left;
     }
-    if (right < size && arr[right] > arr[largest]) {
+    if (right < sizeI && arr[right] > arr[largest]) {
         largest = right;
     }
     if (largest !== i) {
         temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
-        heapify(arr, size, largest);
+        heapify(arr, sizeI, largest);
     }
 }
 function main() {
@@ -45,8 +49,8 @@ function main() {
         52,
         11,
         41,
-        14
-    ];
+        14,
+   ];
     console.log('Input array:', array);
     heapSort(array);
     console.log('Sorted array:', array);
